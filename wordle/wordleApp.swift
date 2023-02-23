0f435cb4099f1a452eb54f79bb3edc31c14b315e
TheNewBadger <83123367+andywoodruff6@users.noreply.github.com>
2023-02-23 17:55:54 -0600

    Initial Commit

---
+++ wordle/wordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordleApp.swift
+//  wordle
+//
+//  Created by Andy W on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct wordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
